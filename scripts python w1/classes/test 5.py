5f503266d71fc75bb41d7dee970675cbfccb2b89
pedro-bezerra-rocha <pedrobr@edu.unifor.br>
2026-08-30 17:29:55 -0300

    Add files via upload

---
+++ scripts python w1/classes/test 5.py
@@ -0,0 +1,11 @@
+n1 = int(input('enter a number:'))
+n2 = int(input('enter another number:'))
+a = n1 + n2
+s = n1 - n2
+m = n1 * n2
+d = n1 / n2
+dd = n1 // n2
+e = n1 ** n2
+print('the sum is: {}, the subtraction is: {} and the multiplication is: {}' .format(a, s, m))
+print('the division is: {:.2f}, the integer division is: {} and the exponentiation is: {}' .format(d, dd, e))
+input('press enter to exit')
