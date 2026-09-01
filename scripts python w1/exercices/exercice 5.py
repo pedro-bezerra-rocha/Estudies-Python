1d949b26048efe3414e271f54b45455952a82140
pedro-bezerra-rocha <pedrobr@edu.unifor.br>
2026-09-01 11:31:53 -0300

    Update exercice 5.py

--- scripts python w1/exercices/exercice 5.py
+++ scripts python w1/exercices/exercice 5.py
@@ -2,5 +2,5 @@ n1 = int(input('enter a number:'))
 n2 = n1 * 2
 n3 = n1 * 3
 n4 = n1 ** (1/2)
-print('the double of {} is: {}, the triple is: {} and the square root is {:.2f}' .format(n1, n2, n3, n4))
+print(f'the double of {n1} is: {n2}, the triple is: {n3} and the square root is {n4:.2f}')
 input('press enter to exit')
