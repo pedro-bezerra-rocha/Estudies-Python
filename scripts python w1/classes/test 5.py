40bfbc07ef862ce7d5ba2797f145bd47481d838a
pedro-bezerra-rocha <pedrobr@edu.unifor.br>
2026-09-01 11:24:10 -0300

    Update test 5.py
    Updated print statements to use f-strings for formatting.

--- scripts python w1/classes/test 5.py
+++ scripts python w1/classes/test 5.py
@@ -6,6 +6,6 @@ m = n1 * n2
 d = n1 / n2
 dd = n1 // n2
 e = n1 ** n2
-print('the sum is: {}, the subtraction is: {} and the multiplication is: {}' .format(a, s, m))
-print('the division is: {:.2f}, the integer division is: {} and the exponentiation is: {}' .format(d, dd, e))
+print('the sum is: {a}, the subtraction is: {s} and the multiplication is: {m}')
+print('the division is: {d:.2f}, the integer division is: {dd:.2f} and the exponentiation is: {e}')
 input('press enter to exit')
