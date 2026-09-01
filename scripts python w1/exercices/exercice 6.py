fa01a6e81b9ec950f78681b654a90a9bd6a1f4c9
pedro-bezerra-rocha <pedrobr@edu.unifor.br>
2026-09-01 11:33:42 -0300

    Update exercice 6.py

--- scripts python w1/exercices/exercice 6.py
+++ scripts python w1/exercices/exercice 6.py
@@ -1,5 +1,5 @@
 n1 = float(input('enter the student´s first grade:'))
 n2 = float(input('enter the student´s second grade:'))
 n3 = (n1 + n2) / 2
-print('the average between {:.2f} and {:.2f} is: {:.2f}'.format (n1, n2, n3))
+print(f'the average between {n1:.2f} and {n2:.2f} is: {n3:.2f}')
 input('press enter to exit')
