b43c92bcaef485bc10e2781aa5235d1231a91a38
pedro-bezerra-rocha <pedrobr@edu.unifor.br>
2026-09-01 11:37:42 -0300

    Update exercice 7.py

--- scripts python w1/exercices/exercice 7.py
+++ scripts python w1/exercices/exercice 7.py
@@ -1,6 +1,6 @@
 n1 = float(input('what´s the width of your wall?'))
 n2 = float(input('what´s the height of your wall?'))
 paint = (n1 * n2) / 2
-print('your wall has the dimension of {:.1f}x{:.1f} and it´s area is: {:.1f}m²' .format(n1, n2, n1 * n2))
-print('to paint this wall you will need {:.1f}L of paint' .format (paint))
+print(f'your wall has the dimension of {n1:.1f}x{n2:.1f} and it´s area is: {n1 * n2:.1f}m²')
+print(f'to paint this wall you will need {paint:.1f}L of paint')
 input('press enter to exit')
