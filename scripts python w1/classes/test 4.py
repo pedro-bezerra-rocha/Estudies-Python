cbceced419e7fe0a0d245dc59cef13e23e637422
pedro-bezerra-rocha <pedrobr@edu.unifor.br>
2026-09-01 11:15:50 -0300

    Refactor sum calculation to use f-strings

--- scripts python w1/classes/test 4.py
+++ scripts python w1/classes/test 4.py
@@ -1,7 +1,7 @@
 n1 = int(input('digit your number:'))
 n2 = int(input('digit other number:'))
 s = n1 +  n2
-print('the sum of {} and {} is {}' .format(n1, n2, s))
+print(f'the sum of {n1} and {n2} is {s}')
 
 
 
